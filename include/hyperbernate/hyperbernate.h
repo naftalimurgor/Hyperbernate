76155d11820e768d335c2fbb74d7657ebcfa09ae
naftalimurgor <murgornaftali@gmail.com>
2023-09-08 14:30:24 +0300

    feat: [wip] hyperbernate module

--- include/hyperbernate/hyperbernate.h
+++ include/hyperbernate/hyperbernate.h
@@ -2,12 +2,19 @@
 #include <string>
 
 namespace Hyperbernate {
-    /**
-     Standard path for 
-    */
-    const std::string ARCH_MEM_SLEEP_PATH = "/sys/power/mem_sleep";
-    bool hasSuspendToDiskSupport();
-    std::string toggleSuspendToDisk();
-    bool hyperbernateEnabled();
+  /**
+   * Standard path for mem_sleep settings supported by ACPI
+
+  */
+  const std::string ARCH_MEM_SLEEP_PATH = "/sys/power/mem_sleep";
+  const std::string LAPTOP_LID_STATE_PATH = "/proc/acpi/button/lid/LID/state";
+  enum LidStatus { CLOSED, OPEN };
+
+  bool hasSuspendToDiskSupport();
+
+  std::string toggleSuspendToDisk();
+
+  bool hyperbernateEnabled();
+  LidStatus getLidStatus();
 
 }  // namespace Hyperbernate
--- source/hyperbernate.cpp
+++ source/hyperbernate.cpp
@@ -1,3 +1,18 @@
 #include <hyperbernate/hyperbernate.h>
+#include<boost/filesystem.hpp>
+#include <string>
 
 using namespace Hyperbernate;
+using namespace boost::filesystem;
+
+bool Hyperbernate::hasSuspendToDiskSupport() const {
+  bool file_exist; = boost::filesystem::file_exists(Hyperbernate::ARCH_MEM_SLEEP_PATH);
+  return file_exists;
+}
+
+std::string Hyperbernate::toggleSuspendToDisk() const {
+    // write to disk
+    // alter the mem_sleep file
+    // listen for lid close events > 5 minutes
+    // toggle hibernate
+}
