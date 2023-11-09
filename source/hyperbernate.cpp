e77a9ba9c39c95e0f1b872de5f95a0f1946f8e9c
naftalimurgor <murgornaftali@gmail.com>
2023-11-09 03:44:20 +0300

    minor refactor

--- source/hyperbernate.cpp
+++ source/hyperbernate.cpp
@@ -1,3 +1,5 @@
+#include "hyperbernate.h"
+
 #include <hyperbernate/hyperbernate.h>
 
 #include <filesystem>
@@ -5,17 +7,17 @@
 
 using namespace Hyperbernate;
 
-bool Hyperbernate::hasSuspendToDiskSupport()
-{
+bool Hyperbernate::hasSuspendToDiskSupport() {
   std::filesystem::path acpi_sleep_path{Hyperbernate::ARCH_MEM_SLEEP_PATH};
   std::filesystem::exists(acpi_sleep_path) ? true : false;
 }
 
-std::string Hyperbernate::toggleSuspendToDisk() 
-{
+std::string Hyperbernate::toggleSuspendToDisk() {
   // write to disk
   // alter the mem_sleep file
   // listen for lid close events > 5 minutes
   // toggle hibernate
   std::filesystem::path acpi_sleep_path{Hyperbernate::ARCH_MEM_SLEEP_PATH};
 }
+
+bool Hyperbernate::hyperbernateEnabled() { return false; }
