607a4414fb308e5b6bb311cf71e861f8ad733e31
naftalimurgor <murgornaftali@gmail.com>
2023-11-09 03:11:57 +0300

    misc: update `hyperbernate.h` code comments

--- include/hyperbernate/hyperbernate.h
+++ include/hyperbernate/hyperbernate.h
@@ -4,10 +4,17 @@
 namespace Hyperbernate {
   /**
    * Standard path for mem_sleep settings supported by ACPI
-
   */
   const std::string ARCH_MEM_SLEEP_PATH = "/sys/power/mem_sleep";
+
+  /*
+   * ACPI laptop lid to monitor state | closed or Open
+  */
   const std::string LAPTOP_LID_STATE_PATH = "/proc/acpi/button/lid/LID/state";
+ 
+  /**
+  * Enum to monitor Lid status, CLOSED = 1, OPEN = 0
+  */
   enum LidStatus { CLOSED, OPEN };
 
   bool hasSuspendToDiskSupport();
